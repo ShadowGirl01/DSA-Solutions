c54536e224ea7ba907c9bd5f05536bf6a36b6e6b
ShadowGirl01 <2005vashu@gmail.com>
2026-01-30 14:34:49 +0530

    Add Rotate string implementation using c++

---
+++ Strings/Rotation/rotateString.cpp
@@ -0,0 +1,24 @@
+//  check whether one string is a rotation of another
+
+//  Input: s = "abcde", goal = "cdeab"
+//  Output: true
+
+//  Time Complexity: O(n)
+//  Space Complexity: O(n)
+
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        
+        if(s.length() != goal.length()){
+            return false;
+        } 
+
+        string temp = s+s;
+        if(temp.find(goal) != string::npos){
+            return true;
+        }
+        return false;
+    }
+};
