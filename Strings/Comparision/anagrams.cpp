2bf6ab9775ef0e2e8b6d3e12e1dcddea3bc5891e
ShadowGirl01 <2005vashu@gmail.com>
2026-02-11 17:14:52 +0530

    Create folders inside LinkedList

--- Strings/Comparision/anagrams.cpp
+++ Strings/Comparision/anagrams.cpp
@@ -33,3 +33,30 @@ int main() {
     cout << "true";
     return 0;
 }
+
+
+//leetcode solution
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        
+        vector<int> count(26,0);
+        for(int i=0;i<s.length();i++){
+            count[s[i]-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            count[t[i]-'a']--;
+        }
+
+        //for all count
+        for(int i=0;i<26;i++){
+            if(count[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
