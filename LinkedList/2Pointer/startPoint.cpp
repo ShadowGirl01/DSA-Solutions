34cbfc9d3b7b4b8133fdfb568afa7e6ebf547bf0
ShadowGirl01 <2005vashu@gmail.com>
2026-02-12 14:14:46 +0530

    Add starting point in LL implementation in c++

---
+++ LinkedList/2Pointer/startPoint.cpp
@@ -0,0 +1,37 @@
+//  142.Find the starting point in LL
+
+//  Input: head = [3,2,0,-4], pos = 1
+//  Output: tail connects to node index 1
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(fast == slow){
+                fast = head;
+                while(fast!=slow){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
