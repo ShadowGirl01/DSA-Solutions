cb19b1d5a2df5b3136afe6e3aad59d2cbde330bd
ShadowGirl01 <2005vashu@gmail.com>
2026-02-12 14:18:22 +0530

    Add detect a cycle in LL implementation in c++

---
+++ LinkedList/2Pointer/detectCycle.cpp
@@ -0,0 +1,33 @@
+//  141. Linked List Cycle
+
+//  Input: head = [3,2,0,-4], pos = 1
+//  Output: true
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != nullptr && fast->next != nullptr ){
+
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
